58ad57de05a3b8ba7b68eb65935c192b823d40e0
P-BUS <pavlo.buslowski@gmail.com>
2023-02-17 14:56:29 +0200

    themes added using compose

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,14 +17,19 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(
-                Message("Android", "Jetpack Compose")
-            )
+            ComposeTutorialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(
+                        Message("Android", "Jetpack Compose")
+                    )
+                }
+            }
         }
     }
 
@@ -41,24 +50,45 @@ class MainActivity : ComponentActivity() {
                     .size(40.dp)
                     // Clip image to be shaped as a circle
                     .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
             )
             // Add a horizontal space between the image and the column
             Spacer(modifier = Modifier.width(8.dp))
 
             Column {
-                Text(text = message.author)
+                Text(
+                    text = message.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2
+                    )
                 // Add a horizontal space between the image and the column
                 Spacer(modifier = Modifier.height(4.dp))
-                Text(text = message.body)
+
+                Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                    Text(
+                        text = message.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.body2
+                    )
+                }
             }
         }
     }
     
-    @Preview
+    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
     @Composable
     fun PreviewMessageCard() {
-        MessageCard(
-            message = Message("Colleague", "Take a look on Jetpack Compose, it`s great!")
-        )
+        ComposeTutorialTheme {
+            Surface {
+                MessageCard(
+                    message = Message("Colleague", "Take a look on Jetpack Compose, it`s great!")
+                )
+            }
+        }
     }
 }
